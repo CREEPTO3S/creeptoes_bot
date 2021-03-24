5d38272126a77ffe31896bfab01b0bd700420b8b
Muhammad Daivasmara Denaw <daivasmara@gmail.com>
2021-03-24 09:58:33 +0700

    [FEATURE] Token Chart

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
 const { CronJob } = require('cron');
 const redis = require('redis');
+const ChartJSImage = require('chart.js-image');
 
 const { TELEGRAM_TOKEN } = process.env;
 
@@ -21,15 +22,21 @@ const fetchCoinList = () => {
   fetch(`${COINGECKO_ENDPOINT}/coins/list`)
     .then((res) => res.json())
     .then((res) => {
-      res.forEach((token) => {
-        client.set(token.symbol, token.id, (err) => {
-          if (err) {
-            console.error(err);
-          }
-        });
-      });
+      const tokens = res.reduce((r, e) => {
+        r.push(e.symbol, e.id);
+        return r;
+      }, []);
 
-      console.log('Caching token list finished!');
+      client.mset(tokens, (err, reply) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        if (reply) {
+          console.log('Caching token list finished!');
+        }
+      });
     })
     .catch((err) => console.error(err));
 };
@@ -73,6 +80,71 @@ bot.onText(/^\/price/, (msg) => {
         });
 
         bot.sendMessage(msg.chat.id, messages.join('\n'));
+      })
+      .catch((error) => {
+        console.error(err);
+        bot.sendMessage(msg.chat.id, error.message);
+      });
+  });
+});
+
+bot.onText(/^\/chart/, (msg) => {
+  const tokenList = msg.text.split(' ');
+  tokenList.shift();
+
+  const firstToken = tokenList.shift();
+
+  console.table({ ...msg.from, token: firstToken });
+
+  client.get(firstToken, (err, reply) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    fetch(`${COINGECKO_ENDPOINT}/coins/${reply}/market_chart?vs_currency=usd&days=7&interval=daily`)
+      .then((res) => res.json())
+      .then(async (res) => {
+        const labels = res.prices.map((price) => new Date(price[0]).toLocaleString().split(',')[0]);
+        const data = res.prices.map((price) => price[1]);
+        const lineChart = new ChartJSImage().chart({
+          type: 'line',
+          data: {
+            labels,
+            datasets: [{
+              label: 'Price',
+              lineTension: 0,
+              backgroundColor: 'rgba(255,+99,+132,+.5)',
+              borderColor: 'rgb(255,+99,+132)',
+              data,
+            }],
+          },
+          options: {
+            title: {
+              display: true,
+              text: `${firstToken.toUpperCase()} - 7 Days Chart`,
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: false,
+                  },
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Price (USD)',
+                  },
+                },
+              ],
+            },
+          },
+        })
+          .backgroundColor('white')
+          .width(1000)
+          .height(300);
+
+        const url = await lineChart.toURL();
+        bot.sendPhoto(msg.chat.id, url);
       })
       .catch((error) => {
         console.error(err);
